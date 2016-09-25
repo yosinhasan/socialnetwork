846aa20ab8a45cc6ed3d580c020e645befa70194
Silver Brain <yosinhasan@gmail.com>
2016-09-25 22:17:31 +0200

    /: added user controller, profile template

---
+++ src/main/java/com/kindhope/web/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.kindhope.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Yosin_Hasan
+ * @version 0.0.1
+ */
+@Controller
+public class UserController {
+    @RequestMapping({"/user/welcome.html", "/user/index.html"})
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("user/welcome");
+    }
+
+}
